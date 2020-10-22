a05f585873237b9be3aeb39d0534bb7cb0e06e0c
Elke Hanschen <elke.hanschen@oetkerdigital.com>
2020-10-22 15:23:32 +0200

    Finished course

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# WIP - Basic JavaScript Tetris Game - Freecodecamp course
-Learn the basics of JavaScript by coding a Tetris game from scratch.
-IDE Atom used in the tutorial video for this (I used PhpStorm).  
+# Basic JavaScript Tetris Game - Freecodecamp YouTube Video
+"Learn the basics of JavaScript by coding a Tetris game from scratch."
+  
+IDE used in the tutorial video: Atom (I used PhpStorm).  
 
 ## Course / Video Contents
 - Introduction  
@@ -42,31 +43,65 @@ IDE Atom used in the tutorial video for this (I used PhpStorm).
 `remove()`  
 `contains()` 
 
-## Game preconditions
-- Based on a grid, 20 squares tall and 10 squares wide  
-- As this makes 200 `<div>s` I've installed [Emmet](https://plugins.jetbrains.com/plugin/7450-emmet-everywhere) to have this created automatically.  
-(A future step could be to try a version where these 200 `<div>`s are created via JavaScript / for loop.)   
+## General info
+- Game is based on a grid, 20 squares tall and 10 squares wide  
+- As this makes 200 `<div>s` I've installed [Emmet](https://plugins.jetbrains.com/plugin/7450-emmet-everywhere) to not create all of them manually.  
 ![Scribble Tetrominos](screenshots/scribble-of-tetrominos.png)  
 
-## PRO
-- Short-ish course  
-- Easy to code along  
+## Rendered Tetromino Arrays
+See screenshots below for better understanding what is happening in `app.js`, from line 23 on.  
 
-## CON
-- Not responsive  
-- Naming  
-- Implicitly declaring variables in course  
-- Using all kinds of weird quotes  
-- Not using spaces, brackets, parenthesis, semicolons properly/consistently  
-- Not everyone knows the term "Tetromino", not even YouTube's CC, so there you get "otra Mina", "Zed touch arena" or even "El techno me know" due to the extreme accent of the tutor  
-
-## Some example screenshots - to be sorted
+All Tetrominos - lTetromino, zTetromino, tTetromino, oTetromino, iTetromino  
 ![All Tetrominos](screenshots/all-tetrominos-01.png)  
+
+All Tetrominos - lTetromino, zTetromino, tTetromino, oTetromino, iTetromino  
 ![All Tetrominos](screenshots/all-tetrominos-02.png)  
-![The Tetrominos](screenshots/theTetrominos-00-01.png)  
-![The Tetrominos](screenshots/theTetrominos-00-02.png)  
+
+lTetromino  
+![The Tetrominos](screenshots/theTetrominos-00-02.png) 
+
+## How to run it
+Right now, this project files are not bundled / do not run with any run command.  
+The `index.html` runs via in IDE configured browser (in my case Chrome).  
+
+Start the game in clicking the Start/Pause button.  
+Use arrow keys to navigate:  
+- Arrow left = move left  
+- Arrow right = move right  
+- Arrow down = move down  
+- Arrow up = turn clockwise  
+
+The little beige grid on the right will show you the next Tetromino falling. 
 
 ## Sources
 [FreeCodeCamp article](https://www.freecodecamp.org/news/learn-javascript-by-creating-a-tetris-game/)  
 [Video: Code Tetris: JavaScript Tutorial for Beginners](https://www.youtube.com/watch?v=rAUn1Lom6dw)  
-[Tetris Basic on GitHub](https://github.com/kubowania/Tetris-Basic)
+[Tetris Basic on GitHub](https://github.com/kubowania/Tetris-Basic) 
+
+# Conclusion
+## Personal Expectation Management towards the course
+- Relied on everything I've encountered via freecodecamp by now, which was mostly very valuable, so when I got this suggestion handed in and saw the length of the course I decided to go for it  
+- Actively coding JavaScript
+- Short (time constraints)
+- Easy to code along
+- Quick to get done
+
+## PRO
+- Short-ish course  
+- Easy to code along  
+- After finishing, the desire to clean up this mess (which I did not do as I want to do something new)
+
+## CON generally
+"This is a great course for people who have never touched code before or people who want to review the fundamentals."  
+- I would never recommend this to someone who has never touched code before as they would not learn anything.  
+- If you look at the content of the course and take into consideration that the course length is 1:36:07 - you quickly figure out yourself that they are rushing through parts and do not explain thoroughly  
+- This also lead to a common use case that in reality, you take way longer that the proclaimed period of time  
+- Accent of tutor: "Tetromino" = not even YouTube's CC was working, so there you get "otra Mina", "touch arena" or even "El techno me know" due to the extreme accent   
+
+## CON technically
+- Not responsive  
+- Naming  
+- Implicitly declaring variables in course  
+- Not using spaces, brackets, parenthesis, quotes, backticks, semicolons properly/consistently  
+- `keyCode` event is deprecated - video is from May 2020, so that's sub optimal to say the least  
+- Generally some code seems messy for the sake of showing quick results
--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-// this is executed after the initial HTML document has been completely loaded and passed
+// code is executed after initial HTML document has been completely loaded and passed
 document.addEventListener('DOMContentLoaded', () => {
     // returns the first HTML element (object) with class .grid
     const grid = document.querySelector('.grid');
-    // returns an array of objects with all HTML elements in .divs grid
+    // returns an array of objects with all HTML elements
     let squares = Array.from(document.querySelectorAll('.grid div'));
-    // returns the first HTML element (object) with ID score
-    const ScoreDisplay = document.querySelector('#score');
-    // returns the first HTML element (object) with ID start-button
-    const StartBtn = document.querySelector('#start-button');
+    // returns the first HTML element (object) with class id score
+    const scoreDisplay = document.querySelector('#score');
+    // returns the first HTML element (object) with class id start-button
+    const startBtn = document.querySelector('#start-button');
     const width = 10;
+    let nextRandom = 0;
+    let timerId;
+    let score = 0;
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
 
-    // The Tetrominos, set up as const as their shapes won't change
+    // Tetrominos
     const lTetromino = [
-        // an array with four items, each array has it's own rotation (= four rotations)
-        // how she did this was not understandable, see README for how the tetrominos come together
+        // array with four array items
         [1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
         [1, width+1, width*2+1, width*2],
@@ -46,37 +55,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // array of all Tetrominos
     const theTetrominos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
-
     let currentPosition = 4;
     let currentRotation = 0;
 
-    // select one of the Tetrominos randomly; first rotation
-    // picks random, nears down to the next integer
-    let random = Math.floor(Math.random()*theTetrominos.length);
-    // will give a random Tetromino of our Tetromino array
+    // select one of the Tetrominos randomly
+    // picks random, neared down to the next integer
+    let random = Math.floor(Math.random() * theTetrominos.length);
     // will always start at the first rotation of any random Tetromino
     let current = theTetrominos[random][currentRotation];
 
-    // draw the randomly chosen Tetromino
+    // draw randomly chosen Tetromino
     function draw() {
-        // forEach applies logic for each item of an array
         current.forEach(index => {
             // each index of the CURRENT array, classList.add to add a class to style
             squares[currentPosition + index].classList.add('tetromino');
+            squares[currentPosition + index].style.backgroundColor = colors[random];
         })
     }
-    // undraw the randomly chosen Tetromino
-    // same as the draw() function but with remove() method
+    // undraw randomly chosen Tetromino
     function undraw() {
-       current.forEach(index => {
-           squares[currentPosition + index].classList.remove('tetromino');
-       })
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino');
+            squares[currentPosition + index].style.backgroundColor = '';
+        })
     }
 
-    // make the Tetromino move down every second
-    let timerId = setInterval(moveDown, 500);
+    // assign key codes to functions
+    // deprecated, was like that in course, todo: it works but fix this
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 38) {
+            rotate();
+        } else if (e.keyCode === 39) {
+            moveRight();
+        } else if (e.keyCode === 40) {
+            moveDown();
+        }
+    }
+    document.addEventListener('keyup', control);
 
-    // move down funtion
+    // move down
     function moveDown() {
         undraw();
         currentPosition += width;
@@ -86,32 +105,126 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // freeze function
     function freeze() {
-        // if this statement is true for at least one of our current items in our array
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-           current.forEach(index => squares[currentPosition + index].classList.add('taken'));
-           // start a new Tetromino falling
-            random = Math.floor(Math.random() * theTetrominos.length);
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            // start a new Tetromino falling
+            random = nextRandom;
+            nextRandom = Math.floor(Math.random() * theTetrominos.length);
             current = theTetrominos[random][currentRotation];
             currentPosition = 4;
             draw();
+            displayShape();
+            addScore();
+            gameOver();
         }
     }
 
-    // move the Tetromino left, unless it's at the edge or there is a blockage
+    // move left, unless it is at the edge or there is a blockage
     function moveLeft() {
+        // remove any trace of the shape in its current location to have a clean plate
         undraw();
-        // so we won't have Tetrominos in 10, 20, 30 etc
+        // define the left edge
         const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
 
         if(!isAtLeftEdge) currentPosition -= 1;
-        // if one of the Tetrominos gets into the space already taken we want to push it back one space
+
+        // we need a taken class; we won't style it, we will assign it to the spaces that have been taken
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition += 1;
         }
-
         draw();
     }
 
+    // move right, unless it is at the edge or there is a blockage
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1);
+        if(!isAtRightEdge) currentPosition += 1;
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
 
+    // rotate
+    function rotate() {
+        undraw();
+        currentRotation ++;
+        // if the current rotation gets to 4, make it go back to 0
+        if(currentRotation === current.length) {
+            currentRotation = 0;
+        }
+        current = theTetrominos[random][currentRotation];
+        draw();
+    }
 
+    // display up-next Tetromino in mini-grid
+    const displaySquares = document.querySelectorAll('.mini-grid div');
+    // tell JS how big our mini-grid is
+    const displayWidth = 4;
+    const displayIndex = 0;
+
+    // the Tetrominos without rotation / original form
+    const upNextTetrominos = [
+        [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], // zTetromino
+        [1, displayWidth, displayWidth+1, displayWidth+2], // tTetromino
+        [0, 1, displayWidth, displayWidth+1], // oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] // iTetromino
+    ]
+
+    // display the shape in the mini-grid
+    function displayShape() {
+        // remove any trace of a Tetromino from the entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino');
+            square.style.backgroundColor = '';
+        })
+        upNextTetrominos[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetromino');
+            displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom];
+        })
+    }
+
+    // add functionality to the button
+    startBtn.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId);
+            timerId = null;
+        } else {
+            draw();
+            timerId = setInterval(moveDown, 1000);
+            nextRandom = Math.floor(Math.random() * theTetrominos.length);
+            displayShape();
+        }
+    })
+
+    // add score
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            // i plus every square that makes up a row
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score += 10;
+                scoreDisplay.innerHTML = score;
+                row.forEach(index => {
+                    squares[index].classList.remove('taken');
+                    squares[index].classList.remove('tetromino');
+                    squares[index].style.backgroundColor = '';
+                })
+                const squaresRemoved = squares.splice(i, width);
+                squares = squaresRemoved.concat(squares);
+                squares.forEach(cell => grid.appendChild(cell));
+            }
+        }
+    }
+
+    // game over
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end';
+            clearInterval(timerId);
+        }
+    }
 })
+
